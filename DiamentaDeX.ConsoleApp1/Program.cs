4c3c7ab150fbcabc86976bb7c01dcfdb47f33b19
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-03-20 16:56:33 -0300

    Adicionar arquivos de projeto.

---
+++ DiamentaDeX.ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DiamentaDeX.ConsoleApp1 {
+    internal class Program {
+        static void Main(string[] args) {
+            {
+                Console.WriteLine("Digite o tamanho do diamante (deve ser um número ímpar): ");
+                int tamanho = Convert.ToInt16(Console.ReadLine());
+
+                if (tamanho % 2 == 0) {
+                    Console.WriteLine("O tamanho do diamante deve ser um número ímpar.");
+                    return;
+                }
+
+                int metade = tamanho / 2;
+
+                for (int i = 0; i < tamanho; i++) {
+                    for (int j = 0; j < tamanho; j++) {
+                        if (i <= metade) {
+                            if (j >= metade - i && j <= metade + i)
+                                Console.Write("X");
+                            else
+                                Console.Write(" ");
+                        }
+                        else {
+                            if (j >= i - metade && j <= tamanho - 1 - (i - metade))
+                                Console.Write("X");
+                            else
+                                Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
